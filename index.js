9ed634076a8a45b004d0395a4bd73d6de5ff7a91
rafsanrjkhan <rjkhan104@gmail.com>
2023-09-21 12:37:19 +0600

    id

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // alert("hiii")
 // $("h1").text("Ki khobor jQuery");
-$(".my-div h1").text("My Div Paragraph through jquery")
+// $(".my-div h1").text("My Div Paragraph through jquery")
+$("#p1").text("Ki khobor p1");
