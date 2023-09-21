ee85deb98fbe14e064b768ddd03dc94132dbba75
rafsanrjkhan <rjkhan104@gmail.com>
2023-09-21 15:21:13 +0600

    append

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // alert("hiii")
 // $("h1").text("Ki khobor jQuery");
 // $(".my-div h1").text("My Div Paragraph through jquery")
-$("#p1").text("Ki khobor p1");
+// $("#p1").text("Ki khobor p1");
+// $("p").html("<b>Goodbye</b>")
+// $("p1").text("Goodbye")
+$("p").append("Goodbye")
+// $("p2").prepend("Goodbye")
